f275e1e2166bb8041996aacb2bd0df096d5a5320
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-04-11 15:17:51 +0200

    add li v-for and js for/ axios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 const { createApp } = Vue
-  
+
 createApp({
-  data() {
-    return {
-      message: 'Hello Vue!',
+    data() {
+        return {
+            url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            emailNumber: 10,
+            randomEmails: []
+        }
+    },
+    mounted() {
+        for (let i = 0; i < this.emailNumber; i++) {
+            axios
+                .get(this.url)
+                .then(response => {
+                    this.randomEmails.push(response.data.response);
+                    console.log(this.randomEmails);
+                })
+        }
     }
-  }
 }).mount('#app')
